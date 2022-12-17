6100e93dc9d00160407c7fe5c076c548f0f0968e
Fatima <bossfatya@gmail.com>
2022-12-17 18:53:32 +0400

    cards almost ready

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let categoryBtn= document.getElementById("categoryBtn")
+let categoryBtn = document.getElementById("categoryBtn");
 
-$( "#categoryBtn" ).click(function() {
-  
-    $( "#active" ).classlist.toggle("slow");
-  });
+$("#categoryBtn").click(function () {
+  $("#active").classlist.toggle("slow");
+});
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
