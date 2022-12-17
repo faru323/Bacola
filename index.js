62ec1b0a963d74b74258d753d2372f095fd0bc89
Fatima <bossfatya@gmail.com>
2022-12-17 19:36:32 +0400

    cards have error

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ $('.autoplay').slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true,
+          loop:false
+      }
+  }
+})
